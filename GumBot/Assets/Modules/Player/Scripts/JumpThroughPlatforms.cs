c59e6b81c52ef3d4a2fb0d65042e9e61e5f3d940
Miles Meacham <littlemeacham@gmail.com>
2015-11-18 18:53:45 -0700

    Got jumping through the bottom of platforms working.

---
+++ GumBot/Assets/Modules/Player/Scripts/JumpThroughPlatforms.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpThroughPlatforms : MonoBehaviour {
+
+	public int playerLayer = 8;
+	public int platformLayer = 9;
+	public float playerVelocity;
+
+	public float newPosition;
+	public float oldPosition;
+	public Transform rb;
+	public Attractor core;
+	
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Transform>();
+		core = FindObjectOfType<Attractor>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//newPosition = rb.position.y;
+
+		newPosition = Vector3.Distance(rb.position, core.GetComponent<Transform>().position);
+
+		playerVelocity = newPosition - oldPosition;
+
+		Physics.IgnoreLayerCollision(playerLayer, platformLayer, playerVelocity > 0.01f);
+
+		oldPosition = newPosition;
+	}
+	
+}
