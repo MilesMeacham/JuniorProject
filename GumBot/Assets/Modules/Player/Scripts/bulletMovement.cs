42700e801d7bdaf20291fcd7ab7a0231cb1e055d
Jeremy Graham <ss2jeremy@gmail.com>
2015-12-03 13:54:33 -0700

    Updated Shard files

--- GumBot/Assets/Modules/Player/Scripts/bulletMovement.cs
+++ GumBot/Assets/Modules/Player/Scripts/bulletMovement.cs
@@ -7,6 +7,11 @@ public class bulletMovement : MonoBehaviour {
     public CharacterMotor2 cm;
     public GameObject TheDude;
     public float lifeDuration;
+    private Attractor Core;
+    public float Xpos;
+    public float Ypos;
+    public float radius;
+
 
     // Use this for initialization
     void Start () {
@@ -20,11 +25,32 @@ public class bulletMovement : MonoBehaviour {
         {
             rb.velocity = transform.TransformDirection(new Vector3(-shotVelocity, 0, 0));
         }
-
+        Core = FindObjectOfType<Attractor>();
+        radius = Vector3.Distance(rb.position, Core.GetComponent<Transform>().position);
     }
 	
 	// Update is called once per frame
 	void Update () {
+        // Xpos = Core.GetComponent<Transform>().position.x + Mathf.Sin*
+        Vector3 Xpointer =  (Core.transform.position - transform.position).normalized;
+        Xpointer.z = 0;
+        float temp = Xpointer.x;
+        Xpointer.x = -Xpointer.y;
+        Xpointer.y = temp;
+     
+
+        if (cm.facingRight == true)
+        {
+            transform.position = transform.position + (Xpointer/5);
+
+        }
+        if (cm.facingRight == false)
+        {
+            transform.position = transform.position + ((Xpointer*-1)/5);
+        }
+
+
+
         StartCoroutine("Destroybullet");
     }
     public IEnumerator Destroybullet()
